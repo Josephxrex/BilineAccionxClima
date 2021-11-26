cdc4812dde4b75438d6a7cb4a4975c17803ede69
Your Name <jc72705@udelasalle.edu.mx>
2021-11-26 16:55:52 -0600

    ArtCasiFin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullArticleComponent } from './components/full-article/full-article.component';
+import { ArticlepageComponent } from './components/articlepage/articlepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //RUTA TEMPORAL SOLO PARA OBESERVAR LOS ARTICULOS
+  {path:'',component:ArticlepageComponent,pathMatch:'full'},
+  {
+    path:'articulos/:variable',
+    component:FullArticleComponent
+  },
+  {path:'**',redirectTo:'articlepage'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlepageComponent } from './components/articlepage/articlepage.component';
+import { FullArticleComponent } from './components/full-article/full-article.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlepageComponent,
+    FullArticleComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
