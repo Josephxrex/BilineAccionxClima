28a8b48b4bf565edb3969f20a874b4c2ebf0ff68
Your Name <jc72705@udelasalle.edu.mx>
2021-12-02 13:56:09 -0600

    RutasyResponsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullArticleComponent } from './components/full-article/full-article.component';
 import { ArticlepageComponent } from './components/articlepage/articlepage.component';
-
 import { GaleriaComponent } from './galeria/galeria.component';
 
 const routes: Routes = [
   {path: 'modal',component: GaleriaComponent},
-  {path:'**',pathMatch:'full', redirectTo: 'modal'},
-  
-  //RUTA TEMPORAL SOLO PARA OBESERVAR LOS ARTICULOS
+  //RUTA TEMPORAL SOLO PARA  LOS ARTICULOS
   {path:'',component:ArticlepageComponent,pathMatch:'full'},
   {
     path:'articulos/:variable',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //Imports de Galeria
 import { GaleriaComponent } from './galeria/galeria.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ArticlepageComponent } from './components/articlepage/articlepage.component';
 import { FullArticleComponent } from './components/full-article/full-article.component';
 
@@ -26,7 +25,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    //NgbModule
 
   ],
   providers: [],
